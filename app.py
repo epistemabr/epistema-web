51d67a97c9be379cfaa40f8aadd061afe64bdcfa
Henrique Soejima <soejima@gmail.com>
2017-07-11 19:18:07 -0300

    changing coming soon text to a value proposition oriented text

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask (__name__)
 
 @app.route('/')
 def index():
-    return "Estamos chegando..."
+    return "Para conectar os pontos entre o trabalho do futuro, a educação do presente e as práticas do passado. Em breve..."
 
 
 if __name__ == "__main__":
