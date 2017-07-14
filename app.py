842bad415c296be992394b3f05fbe37637de43ce
Henrique Soejima <soejima@gmail.com>
2017-07-13 23:15:08 -0300

    reverting value proposition as variable

--- app.py
+++ app.py
@@ -6,8 +6,7 @@ app = Flask (__name__, static_folder='static/', static_url_path='')
 
 @app.route('/')
 def index():
-    value_proposition = "Cocriar uma nova educação, um novo trabalho e novas relações humanas..."
-    return render_template("index.html", vp = value_proposition)
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
